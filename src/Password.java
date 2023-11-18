75c246c898846b62bc5218ead896b2fa659f2064
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-18 23:51:06 +0100

    Initial commit

---
+++ src/Password.java
@@ -0,0 +1,11 @@
+public class Password {
+    String password;
+    int length;
+
+    public Password(String password) {
+        this.password = password;
+        this.length = password.length();
+    }
+
+    
+}

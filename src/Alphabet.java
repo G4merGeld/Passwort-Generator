367dfab98d1dc6f181d681c083ae98af0ea7e33a
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-20 03:21:56 +0100

    added generatePassword method

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -3,4 +3,5 @@ public class Alphabet {
     final static String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     final static String numbers = "0123456789";
     final static String[] mostUsedPasswords = {"123456", "password", "123456789", "12345", "12345678", "qwerty", "1234567", "111111", "1234567890", "123123"};
+    final static String specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; //most used special characters according to: https://owasp.org/www-community/password-special-characters
 }
--- src/Generator.java
+++ src/Generator.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Generator {
@@ -11,18 +12,66 @@ public class Generator {
 
     public Generator(int length) {
         this.length = length;
-        //this.password = password;
-        checkGeneratorConditions();
+        setGeneratorConditions();
+        this.password = generatePassword(length);
     }
-
-
+    //generates password according to given length and conditions for explanation see below:
+    /*
+    * Stringbuilder is used to append characters from the respective set of characters from class Alphabet
+    * for loop to iterate trough each letter
+    * determiner is assigned a random value between 0 (inclusive) and 4 (exclusive)
+    * 0 = append random character from lowercase
+    * 1 = append random character from uppercase
+    * 2 = append random character from numbers
+    * 3 = append random characters from special characters
+    * if number matches a condition that is false counter i is set 1 back
+    * else the for loop continues for the next character
+    * converts StringBuilder genPassword to String and returns it
+    */
     public String generatePassword(int length) {
-        StringBuilder gen = new StringBuilder(length);
-
-        return "error";
+        Random random = new Random();
+        StringBuilder genPassword = new StringBuilder(length);
+        for(int i = 0; i < length; i++) {
+            int determiner = random.nextInt(4);
+            switch (determiner) {
+                case 0:
+                    if(hasLowerCase) {
+                        int alphabetIndex = random.nextInt(Alphabet.lowerCaseLetters.length());
+                        genPassword.append(Alphabet.lowerCaseLetters.charAt(alphabetIndex));
+                    } else {
+                        i--;
+                    }
+                    break;
+                case 1:
+                    if(hasUpperCase) {
+                        int alphabetIndex = random.nextInt(Alphabet.upperCaseLetters.length());
+                        genPassword.append(Alphabet.upperCaseLetters.charAt(alphabetIndex));
+                    } else {
+                        i--;
+                    }
+                    break;
+                case 2:
+                    if(hasNumber) {
+                        int alphabetIndex = random.nextInt(Alphabet.numbers.length());
+                        genPassword.append(Alphabet.numbers.charAt(alphabetIndex));
+                    } else {
+                        i--;
+                    }
+                    break;
+                case 3:
+                    if(hasSpecial) {
+                        int alphabetIndex = random.nextInt(Alphabet.specialCharacters.length());
+                        genPassword.append(Alphabet.specialCharacters.charAt(alphabetIndex));
+                    } else {
+                        i--;
+                    }
+                    break;
+            }
+        }
+        return genPassword.toString();
     }
 
-    private void checkGeneratorConditions() {
+    private void setGeneratorConditions() {
         String y = "Y";
         String n = "N";
         System.out.println("Should generated password include lowercase letters? Y/N");
@@ -91,4 +140,10 @@ public class Generator {
     public boolean getHasSpecial() {
         return hasSpecial;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
